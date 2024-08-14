808e7ea483bc23deb70938ef91b52520e4b114d0
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-08-14 15:00:17 +0200

    Added wrangler

--- src/routes/@[user]/+page.ts
+++ src/routes/@[user]/+page.ts
@@ -3,7 +3,13 @@ import { query } from '$lib/db';
 export async function load({ params, platform }) {
   globalThis.database = platform?.env.database;
   const { user } = params;
-  query(`SELECT * FROM users WHERE username = ?`, user);
+  const users = await query(`SELECT * FROM users WHERE username = ?`, user);
+  if (users.length === 0) {
+    return {
+      status: 404,
+      error: new Error(`Not found`)
+    }
+  };
   return {
     params
   }
